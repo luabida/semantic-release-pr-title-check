f329ae308d0c747b70065cec43b1fcfaac2f2720
Luã Bida Vacaro <luabidaa@gmail.com>
2022-12-13 11:06:44 -0300

    Code improvements

--- src/linter.ts
+++ src/linter.ts
@@ -20,34 +20,33 @@ export function linter(title: string) {
     if (!allowedTags.includes(tag)) {
         throw("❌ Incorrect PR tag.")
     };
-    console.log(tag)
+
     console.log("✅ PR title is correct!");
 };
 
 function extractContext(title: string): [string, string, string] {
 
+    let regEx: RegExp = /(^[\w\s?]+)(\(.+\):\s)([^A-Z\W].*[^.]$)/g;
+
     if (preset === 'conventionalcommits') {
-        var regEx: RegExp = /(^[\w\s?]+)(\(.+\)!:\s)([^A-Z\W].*[^.]$)/g;
-    } else {
-        var regEx: RegExp = /(^[\w\s?]+)(\(.+\):\s)([^A-Z\W].*[^.]$)/g;
-    };   
-     
-    console.log(preset)
-    console.log(regEx)
-    var matches = title.match(regEx) || [];
-    console.log(matches)
+        regEx = /(^[\w\s?]+)(\(.+\)!:\s)([^A-Z\W].*[^.]$)/g;
+    };
+
+    let matches = title.match(regEx) || [];
+
     try {
         let tag = matches.map(e => e.replace(regEx, '$1'))[0];
         let subj = matches.map(e => e.replace(regEx, '$2'))[0];
         let msg = matches.map(e => e.replace(regEx, '$3'))[0];
 
         if (!tag || !subj || !msg) {
-            throw("❌ Incorrect Title.")
+            throw("❌ Incorrect Title.");
         };
 
         return [tag, subj, msg];
 
     } catch (err) {
-        throw(err)
+        console.log(err);
+        throw(err);
     };
 };
