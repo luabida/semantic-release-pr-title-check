98cbbf467b6fc5351b97a96082a85664eeeeca2e
Luã Bida Vacaro <82233055+luabida@users.noreply.github.com>
2022-12-14 14:52:31 -0300

    refactor(linter)!: implementing conventionalcommits option (#19)
    * docs(readme)!: update readme usage version
    
    * refactor(linter)!: implementing conventionalcommits option

--- dist/main.js
+++ dist/main.js
@@ -1,44 +1,20 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.run = void 0;
 const github_1 = require("@actions/github");
-const linter = __importStar(require("./linter"));
+const linter_1 = require("./linter");
 function run() {
     var _a;
     try {
-        const pullRequestTitle = (_a = github_1.context.payload.pull_request) === null || _a === void 0 ? void 0 : _a.title;
+        let pullRequestTitle = (_a = github_1.context.payload.pull_request) === null || _a === void 0 ? void 0 : _a.title;
         if (!pullRequestTitle) {
-            throw ("Title not found");
+            throw ("- Title not found");
         }
         ;
-        linter.linter(pullRequestTitle);
+        (0, linter_1.linter)(pullRequestTitle);
     }
     catch (err) {
-        console.log('❌ PR Title check failed');
-        throw (err);
+        console.log(`❌ PR Title linter failed\n${err}`);
     }
     ;
 }
--- src/linter.ts
+++ src/linter.ts
@@ -1,7 +1,5 @@
 import * as core from "@actions/core";
 
-
-const preset: string = core.getInput('convention-name');
 const allowedTags = [
     "build",
     "ci",
@@ -14,39 +12,44 @@ const allowedTags = [
     "test",
 ];
 
+
 export function linter(title: string) {
-    let [tag, subj, msg] = extractContext(title);
 
-    if (!allowedTags.includes(tag)) {
-        throw("❌ Incorrect PR tag.")
-    };
-
-    console.log("✅ PR title is correct!");
-};
-
-function extractContext(title: string): [string, string, string] {
-
-    let regEx: RegExp = /(^[\w\s?]+)(\(.+\):\s)([^A-Z\W].*[^.]$)/g;
-
-    if (preset === 'conventionalcommits') {
-        regEx = /(^[\w\s?]+)(\(.+\)!:\s)([^A-Z\W].*[^.]$)/g;
-    };
-
-    let matches = title.match(regEx) || [];
+    let preset: string = core.getInput('convention-name');
 
     try {
-        let tag = matches.map(e => e.replace(regEx, '$1'))[0];
-        let subj = matches.map(e => e.replace(regEx, '$2'))[0];
-        let msg = matches.map(e => e.replace(regEx, '$3'))[0];
+        let splitTitle = extractContext(title, preset);
+        let tag = splitTitle[0];
 
-        if (!tag || !subj || !msg) {
-            throw("❌ Incorrect Title.");
+        if (!allowedTags.includes(tag)) {
+            throw(`- Incorrect PR tag: "${tag}" is not accepted by Semantic-Release`)
         };
 
-        return [tag, subj, msg];
+        console.log("✅ PR title is correct!");
 
     } catch (err) {
-        console.log(err);
-        throw(err);
+        console.log("❌ Incorrect PR title.")
+        throw(err)
+    };
+};
+
+
+function extractContext(title: string, preset: string): string[] {
+
+    let regEx: RegExp = /(^[\w\s?]+)(\(.+\)(!?):\s)([^A-Z\W].*[^.]$)/g;
+    
+    let matches = title.matchAll(regEx);
+
+    try {
+        let results = Array.from(matches)[0].filter(Boolean).splice(1);
+
+        if (results.length === 4 && preset !== 'conventionalcommits') {
+            throw("- To use '!' in the title, set preset as `convenvionalcommits`");
+        };
+
+        return results;
+
+    } catch (err) {
+        throw(`${err}\n- "${title}" format is incorrect. Please use Angular Commit Message Conventions`)
     };
 };
